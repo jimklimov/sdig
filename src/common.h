802e68ea9a8c0058e65d021533e4713335f3423e
Jim Klimov <1636756+jimklimov@users.noreply.github.com>
2024-02-15 10:28:37 +0000

    src/*.h: add header guards

--- src/common.h
+++ src/common.h
@@ -1,3 +1,5 @@
+#ifndef SDIG_COMMON_H
+
 #define LARGEBUF 1024
 
 void debug(int level, const char *format, ...);
@@ -11,3 +13,6 @@ int parseconf(const char *fn, int ln, char *buf, char **arg, int numargs);
 int snprintfcat(char *dst, size_t size, const char *fmt, ...);
 void *xmalloc(size_t size);
 char *xstrdup(const char *string);
+
+#define SDIG_COMMON_H 1
+#endif
--- src/snmpget.h
+++ src/snmpget.h
@@ -2,6 +2,11 @@
  * $Id$
  */
 
+#ifndef SDIG_SNMPGET_H
+
 int snmpget_int(char *host, char *community, char *reqoid);
 char *snmpget_mac(char *host, char *community, char *reqoid);
 char *snmpget_str(char *host, char *community, char *reqoid);
+
+#define SDIG_SNMPGET_H 1
+#endif
